b8355df949a7ec350c46a9e75da74ff6ae4d4955
ClaudiaDaneder <claudia.daneder@hotmail.com>
2024-06-17 17:13:33 +0200

    serializer edited again

--- customers/serializers.py
+++ customers/serializers.py
@@ -4,26 +4,15 @@ from .models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'street', 'zip_code', 'city', 'country')
-        extra_kwargs = {
-            'username': {
-                'required': True
-                },
-            'first_name': {
-                'required': True
-                },
-            'last_name': {
-                'required': True
-                },
-            'email': {
-                'required': True
-                },
-            'password': {
-                'write_only': True
-                }
-            }
-
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = Customer.objects.create_user(**validated_data)
+        user = Customer.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
