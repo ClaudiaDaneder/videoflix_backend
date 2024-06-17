1b49256ae16bc57199a4f48fe7ead2cc13fbf046
root <root@instance-20240614-103801.europe-west6-a.c.videoflix-426207.internal>
2024-06-17 11:09:50 +0000

    tweaked settings on server

--- content/tasks.py
+++ content/tasks.py
@@ -5,16 +5,17 @@ def convert480p(source):
     base, ext = os.path.splitext(source)
     target = base + '_480p' + ext
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
-    subprocess.run(cmd)
+    subprocess.run(cmd, shell=True)
 
 def convert720p(source):
     base, ext = os.path.splitext(source)
     target = base + '_720p' + ext
     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
-    subprocess.run(cmd)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print(result.stderr)
 
 
 def getThumbnail(source, target):
     os.makedirs(os.path.dirname(target), exist_ok=True)
     cmd = 'ffmpeg -i "{}" -ss 00:00:01.000 -update true -frames:v 1 "{}"'.format(source, target)
-    subprocess.run(cmd)
+    subprocess.run(cmd, shell=True)
