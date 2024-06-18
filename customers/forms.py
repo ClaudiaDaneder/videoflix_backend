e94ab37f136375478a18e171e2fa4ea0b40bf055
ClaudiaDaneder <claudia.daneder@hotmail.com>
2024-06-18 23:12:54 +0200

    changed folder structure

--- customers/forms.py
+++ customers/forms.py
@@ -4,4 +4,4 @@ from .models import Customer
 class CustomerCreationForm(UserCreationForm):
     class Meta:
         model = Customer
-        # fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
