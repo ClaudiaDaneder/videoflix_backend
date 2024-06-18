ac6e114aaefb32e7494e660a6d6f93e82395669f
ClaudiaDaneder <claudia.daneder@hotmail.com>
2024-06-18 17:41:20 +0200

    changed folder structure

--- content/tasks.py
+++ content/tasks.py
@@ -12,7 +12,7 @@ def convert360p(source, video_id):
     base, ext = os.path.splitext(source)
     target = 'media/videos_uploaded/360p/' + os.path.basename(base) + '_360p' + ext
     os.makedirs(os.path.dirname(target), exist_ok=True)
-    cmd = 'ffmpeg -i "{}" -s hd360 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
+    cmd = 'ffmpeg -i "{}" -s 640x360 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
     subprocess.run(cmd, shell=True)
     video = Video.objects.get(id=video_id)
     video.video_360p_path = target
